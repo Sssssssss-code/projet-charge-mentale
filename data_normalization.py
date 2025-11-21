6d454c51bc2d695cdfedbc0cd1b2722d509604db
redzebra42 <delaigletristan42@gmail.com>
2025-11-21 16:12:36 +0100

    normalize all data

--- data_normalization.py
+++ data_normalization.py
@@ -1,19 +1,20 @@
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
 import os
 
 
 def normalise_data():
+    min_max_scaler = MinMaxScaler()
     path = 'data/MAUS/Data/Raw_data'
-    normalized_data_path = 'data/MAUS/Data/Normalied_data'
+    normalized_data_path = 'data/MAUS/Data/Normalized_data'
     for folder_name in os.listdir(path):
         for file in os.listdir(path + '/' + folder_name):
             if file[-3:] == 'csv':
                 df = pd.read_csv(f'{path}/{folder_name}/{file}')
             else:
                 df = pd.read_excel(f'{path}/{folder_name}/{file}')
-            df = normalize(df) #TODO this transforms into a numpy array (so no to_csv)
-            df.to_csv(f'{normalized_data_path}/{folder_name}/normalized_{file}')
+            df = pd.DataFrame(min_max_scaler.fit_transform(df))
+            df.to_csv(f'{normalized_data_path}/{folder_name}_normalized_{file}')
             
         
 
